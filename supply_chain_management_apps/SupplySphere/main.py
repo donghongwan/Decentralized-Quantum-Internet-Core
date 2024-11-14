087569172c17c768248307bf2907f3f9bf5b78f9
KOSASIH <kosasihg88@gmail.com>
2024-11-14 21:23:46 +0700

    Create main.py

---
+++ supply_chain_management_apps/SupplySphere/main.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_socketio import SocketIO
+from models import db
+from controllers import user_controller, shipment_controller, inventory_controller, analytics_controller
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///supply_sphere.db'
+app.config['SECRET_KEY'] = 'your_secret_key'
+db.init_app(app)
+
+socketio = SocketIO(app)
+
+# Register Blueprints
+app.register_blueprint(user_controller.bp)
+app.register_blueprint(shipment_controller.bp)
+app.register_blueprint(inventory_controller.bp)
+app.register_blueprint(analytics_controller.bp)
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    socketio.run(app)
