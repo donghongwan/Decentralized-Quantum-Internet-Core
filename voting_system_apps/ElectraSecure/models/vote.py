a883d773950671598cb50690b3efb57a42258cf7
KOSASIH <kosasihg88@gmail.com>
2024-11-14 19:46:59 +0700

    Create vote.py

---
+++ voting_system_apps/ElectraSecure/models/vote.py
@@ -0,0 +1,6 @@
+class Vote:
+    def __init__(self, vote_id, user_id, election_id, candidate_id):
+        self.vote_id = vote_id
+        self.user_id = user_id
+        self.election_id = election_id
+        self.candidate_id = candidate_id
