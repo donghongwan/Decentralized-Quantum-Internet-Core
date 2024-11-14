01b156de2895535b746be946b4b8981890ea2b13
KOSASIH <kosasihg88@gmail.com>
2024-11-14 17:00:52 +0700

    Create user.py

---
+++ healthcare_apps/WellnessNexus/models/user.py
@@ -0,0 +1,7 @@
+class User:
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+        self.fitness_data = []
+        self.nutrition_data = []
+        self.mental_health_data = []
