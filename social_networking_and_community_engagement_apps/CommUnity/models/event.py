cdc5c4e958df4dbebdd86e31190bab9ac2b63fe4
KOSASIH <kosasihg88@gmail.com>
2024-11-14 20:41:15 +0700

    Create event.py

---
+++ social_networking_and_community_engagement_apps/CommUnity/models/event.py
@@ -0,0 +1,12 @@
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(500))
+    date = db.Column(db.DateTime, nullable=False)
+    group_id = db.Column(db.Integer, db.ForeignKey('group.id'))
+    attendees = db.relationship('User ', secondary='event_attendees')
+
+class EventAttendees(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    event_id = db.Column(db.Integer, db.ForeignKey('event.id'))
