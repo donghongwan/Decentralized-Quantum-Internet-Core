d2490be43178072436335f467150bfa4717d23a3
KOSASIH <kosasihg88@gmail.com>
2024-11-14 20:41:47 +0700

    Create resource.py

---
+++ social_networking_and_community_engagement_apps/CommUnity/models/resource.py
@@ -0,0 +1,5 @@
+class Resource(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(500))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
