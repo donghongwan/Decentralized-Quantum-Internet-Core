1c5dffa5666d223bacb6ae9f49c80085f59b9ff1
KOSASIH <kosasihg88@gmail.com>
2024-11-14 17:51:22 +0700

    Create chat_service.py

---
+++ secure_chat_apps/SecureLink/services/chat_service.py
@@ -0,0 +1,8 @@
+class ChatService:
+    def send_message(self, sender, receiver, content):
+        # Logic to send a message securely
+        pass
+
+    def retrieve_messages(self, user):
+        # Logic to retrieve messages for a user
+        pass
