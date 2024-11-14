36bd22407ab12e7e7622c6242d329a2cb6c70e3b
KOSASIH <kosasihg88@gmail.com>
2024-11-14 17:51:54 +0700

    Create video_service.py

---
+++ secure_chat_apps/SecureLink/services/video_service.py
@@ -0,0 +1,8 @@
+class VideoService:
+    def start_video_call(self, participants):
+        # Logic to initiate a secure video call
+        pass
+
+    def end_video_call(self, call):
+        # Logic to end a video call
+        pass
