fcb8a3037f3cbdf0f961b7491bd7d487b6706663
KOSASIH <kosasihg88@gmail.com>
2024-11-14 17:27:14 +0700

    Create fitness.py

---
+++ healthcare_apps/WellnessNexus/models/fitness.py
@@ -0,0 +1,5 @@
+class Fitness:
+    def __init__(self, activity, duration, calories_burned):
+        self.activity = activity
+        self.duration = duration  # in minutes
+        self.calories_burned = calories_burned
