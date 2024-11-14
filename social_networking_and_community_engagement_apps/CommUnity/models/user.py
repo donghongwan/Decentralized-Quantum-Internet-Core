29d53a1e166f822beb4957aa2617ee0b79eb07a6
KOSASIH <kosasihg88@gmail.com>
2024-11-14 20:40:02 +0700

    Create user.py

---
+++ social_networking_and_community_engagement_apps/CommUnity/models/user.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+
+    def create_token(self):
+        return create_access_token(identity=self.id)
