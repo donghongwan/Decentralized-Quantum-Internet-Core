b915cc80c816faf7b31a4c84b763cce42d2ce90c
KOSASIH <kosasihg88@gmail.com>
2024-11-14 17:50:04 +0700

    Create video_call.py

---
+++ secure_chat_apps/SecureLink/models/video_call.py
@@ -0,0 +1,5 @@
+class VideoCall:
+    def __init__(self, participants, start_time, duration):
+        self.participants = participants
+        self.start_time = start_time
+        self.duration = duration
