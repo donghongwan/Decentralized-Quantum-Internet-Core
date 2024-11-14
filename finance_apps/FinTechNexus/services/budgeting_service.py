6a26daafd322fc5bb44f2a14d885dd516a0968cd
KOSASIH <kosasihg88@gmail.com>
2024-11-14 17:15:17 +0700

    Create budgeting_service.py

---
+++ finance_apps/FinTechNexus/services/budgeting_service.py
@@ -0,0 +1,8 @@
+class BudgetingService:
+    def create_budget(self, user, amount):
+        # Logic to create a budget
+        pass
+
+    def track_expenses(self, user):
+        # Logic to track expenses
+        pass
