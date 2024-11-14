05cd9c0a4cb739d666bad820f74c8932e7726fb0
KOSASIH <kosasihg88@gmail.com>
2024-11-14 21:32:00 +0700

    Create inventory.py

---
+++ supply_chain_management_apps/SupplySphere/models/inventory.py
@@ -0,0 +1,7 @@
+from models import db
+
+class Inventory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    location = db.Column(db.String(100), nullable=False)
