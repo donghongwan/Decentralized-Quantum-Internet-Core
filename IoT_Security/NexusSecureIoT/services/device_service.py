2da57992657370b2bd09eae787f1e6f4f600d1c5
KOSASIH <kosasihg88@gmail.com>
2024-11-14 19:24:26 +0700

    Create device_service.py

---
+++ IoT_Security/NexusSecureIoT/services/device_service.py
@@ -0,0 +1,8 @@
+class DeviceService:
+    def register_device(self, device):
+        # Logic to register a device
+        pass
+
+    def update_device_status(self, device_id, status):
+        # Logic to update device status
+        pass
