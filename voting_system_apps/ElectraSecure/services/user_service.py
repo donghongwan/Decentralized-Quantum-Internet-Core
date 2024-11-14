db92fe9f8389694fa242920dcf3a5b1ffd166598
KOSASIH <kosasihg88@gmail.com>
2024-11-14 19:48:41 +0700

    Create user_service.py

---
+++ voting_system_apps/ElectraSecure/services/user_service.py
@@ -0,0 +1,8 @@
+class UserService:
+    def register_user(self, username, password):
+        # Logic to register a new user
+        pass
+
+    def authenticate_user(self, username, password):
+        # Logic to authenticate a user
+        pass
