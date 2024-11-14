8b5b0c5649c25f8c39273155205d298ed655f363
KOSASIH <kosasihg88@gmail.com>
2024-11-14 19:49:20 +0700

    Create voting_service.py

---
+++ voting_system_apps/ElectraSecure/services/voting_service.py
@@ -0,0 +1,8 @@
+class VotingService:
+    def cast_vote(self, user_id, election_id, candidate_id):
+        # Logic to cast a vote
+        pass
+
+    def get_results(self, election_id):
+        # Logic to get election results
+        pass
