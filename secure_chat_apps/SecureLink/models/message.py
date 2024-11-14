b9c01d0933d5fe03c66ec3496f7c66c7d0a8dda9
KOSASIH <kosasihg88@gmail.com>
2024-11-14 17:47:46 +0700

    Create message.py

---
+++ secure_chat_apps/SecureLink/models/message.py
@@ -0,0 +1,6 @@
+class Message:
+    def __init__(self, sender, receiver, content, timestamp):
+        self.sender = sender
+        self.receiver = receiver
+        self.content = content
+        self.timestamp = timestamp
