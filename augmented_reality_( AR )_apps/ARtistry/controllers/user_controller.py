f54f3e2a4f60c7ecc2ad183573ab76360795cbd6
KOSASIH <kosasihg88@gmail.com>
2024-11-15 16:35:18 +0700

    Create user_controller.py

---
+++ augmented_reality_( AR )_apps/ARtistry/controllers/user_controller.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, request, jsonify
+from services.user_service import UserService
+
+bp = Blueprint('user', __name__)
+
+@bp.route('/register', methods=['POST'])
+def register():
+    data = request.json
+    user = UserService.register_user(data['username'], data['email'], data['password'])
+    return jsonify({'id': user.id}), 201
+
+@bp.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    token = UserService.authenticate_user(data['email'], data['password'])
+    if token:
+        return jsonify({'token': token}), 200
+    return jsonify({'message': 'Invalid credentials'}), 401
