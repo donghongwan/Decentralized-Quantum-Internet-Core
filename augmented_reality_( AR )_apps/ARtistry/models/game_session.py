618908f66cf43d0a1932de53b8c66a151ee460ef
KOSASIH <kosasihg88@gmail.com>
2024-11-15 16:31:26 +0700

    Create game_session.py

---
+++ augmented_reality_( AR )_apps/ARtistry/models/game_session.py
@@ -0,0 +1,7 @@
+from models import db
+
+class GameSession(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    score = db.Column(db.Integer, nullable=False)
+    duration = db.Column(db.Float, nullable=False)
