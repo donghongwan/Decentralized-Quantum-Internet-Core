0101c6d1fa2315d1b762ebd13e6c21e8a020f15a
KOSASIH <kosasihg88@gmail.com>
2024-11-15 14:30:46 +0700

    Create course.py

---
+++ education_and_e-learning_apps/SmartIQ/models/course.py
@@ -0,0 +1,7 @@
+from models import db
+
+class Course(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+    content = db.Column(db.Text, nullable=False)
